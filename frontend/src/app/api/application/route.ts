59b17695845b4e651e02842dae7945f6c79b8646
sakinstitutes <sakinstitutes@gmail.com>
2026-08-02 10:49:18 +0530

    formspree end point

--- frontend/src/app/api/application/route.ts
+++ frontend/src/app/api/application/route.ts
@@ -1,7 +1,7 @@
 export const dynamic = 'force-dynamic';
 import { NextResponse } from 'next/server';
 
-const FORMSPREE_ENDPOINT = 'https://formspree.io/f/meeykddj';
+const FORMSPREE_ENDPOINT = 'https://formspree.io/f/xbdnvwon';
 
 export async function POST(request: Request) {
   try {
